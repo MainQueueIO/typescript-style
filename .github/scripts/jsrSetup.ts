6ee4a20ec661a5e5038c5ba94da4369feb857169
Dieguinho Pinho <diegocuehdz@gmail.com>
2025-07-23 03:00:51 -0600

    style: format github folders

--- .github/scripts/jsrSetup.ts
+++ .github/scripts/jsrSetup.ts
@@ -1,26 +1,28 @@
-
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from 'fs';
 
 const args = Bun.argv;
 
-const versionFlag = args.find(arg => arg.startsWith("--version="));
+const versionFlag = args.find((arg) => arg.startsWith('--version='));
 
 let version: string | null = null;
-if (versionFlag && versionFlag.split("=") && versionFlag.split("=").length > 1) {
-  version = versionFlag.split("=")[1];
+if (
+  versionFlag &&
+  versionFlag.split('=') &&
+  versionFlag.split('=').length > 1
+) {
+  version = versionFlag.split('=')[1];
 }
 
 if (!version) {
-  const pkgPath = new URL("../../package.json", import.meta.url).pathname;
-  const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
+  const pkgPath = new URL('../../package.json', import.meta.url).pathname;
+  const pkg = JSON.parse(readFileSync(pkgPath, 'utf-8'));
   version = pkg.version;
 }
 
-
-const jsrPath = new URL("../../jsr.json", import.meta.url).pathname;
-const jsr = JSON.parse(readFileSync(jsrPath, "utf-8"));
+const jsrPath = new URL('../../jsr.json', import.meta.url).pathname;
+const jsr = JSON.parse(readFileSync(jsrPath, 'utf-8'));
 jsr.version = version;
 
-writeFileSync(jsrPath, JSON.stringify(jsr, null, 2) + "\n");
+writeFileSync(jsrPath, JSON.stringify(jsr, null, 2) + '\n');
 
 console.log(version);
