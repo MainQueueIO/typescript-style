244cad8104769220c41fb88e4886e4fa78bb0055
Dieguinho Pinho <29768792+diegocuehdz@users.noreply.github.com>
2025-11-03 01:15:16 -0600

    feat: configure vue config correctly

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import eslintConfig from './eslint.config.mjs';
+import eslintVueConfig from './eslint-vue.config.mjs';
 import oxlintConfig from './oxlint-base.json';
 import prettierConfig from './prettier.config.mjs';
 
-export { eslintConfig, oxlintConfig, prettierConfig };
+export { eslintConfig, eslintVueConfig, oxlintConfig, prettierConfig };
 
 export default {
   eslint: eslintConfig,
+  eslintVue: eslintVueConfig,
   oxlint: oxlintConfig,
   prettier: prettierConfig,
 };
