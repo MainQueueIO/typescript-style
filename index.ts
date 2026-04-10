cb529f390fe5aa158759a94898efc73c8473571a
Diego Cué Hernández <29768792+diegocuehdz@users.noreply.github.com>
2026-04-09 20:12:15 -0600

    feat(oxfmt): add oxc's formatter config for better performance (#40)

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import eslintConfig from './eslint.config.mjs';
 import eslintVueConfig from './eslint-vue.config.mjs';
+import oxfmt from './.oxfmtrc.json';
 import oxlintConfig from './oxlint-base.json';
 import prettierConfig from './prettier.config.mjs';
 
-export { eslintConfig, eslintVueConfig, oxlintConfig, prettierConfig };
+export { eslintConfig, eslintVueConfig, oxfmt, oxlintConfig, prettierConfig };
 
 export default {
   eslint: eslintConfig,
   eslintVue: eslintVueConfig,
+  oxfmt: oxfmt,
   oxlint: oxlintConfig,
   prettier: prettierConfig,
 };
